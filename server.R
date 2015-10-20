0581d3dd7a4a1580b8323f49e8df8ccf22295664
Wat Hughes <wat.hughes@gmail.com>
2015-10-19 22:20:05 -0400

    First working version

--- server.R
+++ server.R
@@ -2,11 +2,90 @@
 # This is thee user interface definitions for the Shiny Application portion of
 # the course project for Johns Hopkins' (via Coursera) Developing Data Products.
 
+library(utils)
+library(data.table)
+library(rvest) # XML/HTML handling
+library(rdrop2) # Dropbox wrapper
 library(shiny)
+
+library(shinyTree)
+
+source('MakeCodeTree.R')
+source('CondLoadDataTable.R')
+
+CondLoadDataTable('SeriesYearIndustryCounts')
 
 shinyServer(function(input, output)
 {
+    # Injury Predictions by Industry
+    datasetInputP = reactive({
+        CondLoadDataTable(input$datasetP)
+    })
 
+    output$plot1P = renderPlot({
+        ds = datasetInputP() # For the reactivity
+        selTxtP = SelectionDisplayText()
+        selCodeP = sub(':.*','',selTxtP)
+        selData = SeriesYearIndustryCounts[industry_code==selCodeP]
+        if (nrow(selData) > 0)
+        {
+            selMod = lm(N~year,data=selData)
+            predCount = input$yearsP
+            predYears = data.frame(year=c(1:predCount))
+            predYears$year = predYears$year + 2013
+            selPred = predict(selMod,predYears)
+            df=data.frame(year=c(selData$year,predYears$year),SeriesCount=c(selData$N,selPred),DataOrPrediction=as.factor(c(rep(1,3),rep(2,predCount))))
+            plot(SeriesCount~year,data=df,col=DataOrPrediction,pch=9,cex=2,xlab='Year. BLS data in black and predictions in red.',ylab='Series Count')
+        }
+    })
+    output$treeP <- renderTree({
+        ds = datasetInputP() # For the reactivity
+        datasetname = input$datasetP
+        ct = FNsP[[datasetname]]
+        if (!is.list(ct))
+        {
+            ct = MakeCodeTree(get(datasetname),4) # display_level >= 4 will be ignored
+            FNsP[[datasetname]] <<- ct
+        }
+        DisplayTree = ct$GetDisplayTree()
+        # browser() # Breakpoints seem flaky in Shiny
+        DisplayTree
+    })
+    SelectionDisplayText = reactive({
+        ds = datasetInputP() # For the reactivity
+        datasetname = input$datasetP
+        tree = input$treeP
+        if (is.null(tree))
+        {
+            'None'
+        } else
+        {
+            sel = get_selected(tree,format='slices')
+            # List of 1 # Slices Format: all lists, no attributes.
+            #  $ :List of 1
+            #   ..$ All workers:List of 1
+            #   .. ..$ Service-providing:List of 1
+            #   .. .. ..$ Professional and business services: num 0
+            ct = FNsP[[datasetname]]
+            CodeDef = ct$GetSelectedCodeDef(sel)
+            DisplayTextColumn = ct$GetDisplayTextColumn()
+            CodeColumn = ct$GetCodeColumn()
+            if (is.null(CodeDef))
+            {
+                selTxtP = 'Nothing selected'
+            }
+            else
+            {
+                selTxtP = paste0(CodeDef[1,CodeColumn,with=F],':  ',CodeDef[1,DisplayTextColumn,with=F])
+            }
+            selTxtP
+        }
+    })
+    output$selTxtP <- renderText({
+        selTxtP = SelectionDisplayText()
+    })
+
+    # RStudio default application server code
     output$distPlot <- renderPlot({
 
         # generate bins based on input$bins from ui.R
@@ -19,3 +98,4 @@ shinyServer(function(input, output)
     })
 
 }) # shinyServer(function
+
--- ui.R
+++ ui.R
@@ -4,10 +4,91 @@
 
 library(shiny)
 
+library(shinyTree)
+
+# Globals are first computed or just plain set to parameterize behavior.
+
+hostname = system('hostname', intern=T)
+
+if (hostname == 'AJ')
+{
+    MaxRowsToRead <<- 10000 # data max is less than 50M
+    DDLRoot = 'E:/wat/misc/DDL'
+    DataDir = paste0(DDLRoot,'/Data')
+} else
+{
+    MaxRowsToRead <<- 100000000 # data max is less than 50M so this gets all
+}
+if (hostname == 'VM-EP-3')
+{
+    DDLRoot = 'd:/RProjects' # Oops
+    DataDir = paste0(DDLRoot,'/RedLineData')
+}
+ForceListRLFilesFromDropBox = F # T to locally test DropBox connectivity
+if (!ForceListRLFilesFromDropBox & (hostname == 'VM-EP-3' | hostname == 'AJ'))
+{
+    ListRLFilesFromDropBox = FALSE # Local only
+    QuietDownload <<- FALSE
+} else
+{
+    DataDir = 'Data'
+    ListRLFilesFromDropBox = TRUE # Any local cache is still used for the actual data
+    QuietDownload <<- TRUE
+}
+OrigDataDir <<- paste0(DataDir,'/BLSOrig')
+CompressedRDataDir <<- paste0(DataDir,'/CompressedRDA')
+dir.create(OrigDataDir,recursive=T,showWarnings=F)
+dir.create(CompressedRDataDir,recursive=T,showWarnings=F)
+
+# If we don't have the data locally, check DropBox using these globals.
+
+DropBoxDataDir = '/Data'
+DropBoxCompressedRDataDir <<- paste0(DropBoxDataDir,'/CompressedRDA')
+# This works around an apparent limitation of publishing to shinyapps.io:
+if (!file.exists('.httr-oauth') & file.exists('httr-oauth')) {file.rename('httr-oauth','.httr-oauth')}
+
+# Cache the filelist from BLS into FNsB
+
+BLSDataURL <<- 'http://download.bls.gov/pub/time.series/cs'
+
+FNsP <<- list(
+    rl.industry4=''
+)
+
 shinyUI(fluidPage(
     tabsetPanel
     (
         type = "tabs",
+        tabPanel
+        (
+            'Injury Predictions by Industry',
+            sidebarLayout
+            (
+                sidebarPanel
+                (
+                    conditionalPanel
+                    (
+                        condition='false',
+                        selectInput
+                        (
+                            'datasetP',
+                            'Choose a data.table:',
+                            choices = names(FNsP)
+                        )
+                    ),
+                    numericInput('yearsP','Number of years to predict:',3,min=1,max=9)
+                ),
+                mainPanel
+                (
+                    h3('Injuries (Series) by Year for the Selected Industry'),
+                    plotOutput('plot1P'),
+                    h3('Currently Selected:'),
+                    verbatimTextOutput('selTxtP'),
+                    hr(),
+                    shinyTree('treeP')
+                )
+            )
+        ), # tabPanel
         tabPanel
         (
             'RStudio Default Application',
@@ -34,3 +115,4 @@ shinyUI(fluidPage(
         ) # tabPanel
     ) # tabsetPanel
 )) # shinyUI(fluidPage
+
