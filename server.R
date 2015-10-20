cce17e98fdb42fccf97310cde6282ccf3c64902b
Wat Hughes <wat.hughes@gmail.com>
2015-10-19 21:47:03 -0400

    Formatting, tabset

--- server.R
+++ server.R
@@ -1,23 +1,21 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# This is thee user interface definitions for the Shiny Application portion of
+# the course project for Johns Hopkins' (via Coursera) Developing Data Products.
 
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output)
+{
 
-  output$distPlot <- renderPlot({
+    output$distPlot <- renderPlot({
 
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
-  })
+    })
 
-})
+}) # shinyServer(function
--- ui.R
+++ ui.R
@@ -1,30 +1,36 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# This is thee user interface definitions for the Shiny Application portion of
+# the course project for Johns Hopkins' (via Coursera) Developing Data Products.
 
 library(shiny)
 
 shinyUI(fluidPage(
+    tabsetPanel
+    (
+        type = "tabs",
+        tabPanel
+        (
+            'RStudio Default Application',
+            # Application title
+            titlePanel("Old Faithful Geyser Data"),
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+            # Sidebar with a slider input for number of bins
+            sidebarLayout
+            (
+                sidebarPanel
+                (
+                    sliderInput("bins",
+                    "Number of bins:",
+                    min = 1,
+                    max = 50,
+                    value = 30)
+                ),
+                # Show a plot of the generated distribution
+                mainPanel
+                (
+                    plotOutput("distPlot")
+                )
+            )
+        ) # tabPanel
+    ) # tabsetPanel
+)) # shinyUI(fluidPage
